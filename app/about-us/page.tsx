309a5936517e70831baaeb83306fda5151e31bb4
Justin Baker <jbaker2016@fau.edu>
2023-12-25 06:20:03 -0500

    fixed apostraphe

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -7,7 +7,7 @@ export default function AboutUs() {
         </h2>
 
         <p className=" leading-relaxed text-gray-500 xl:text-lg mt-4">
-        Hi, I'm Nidhi Suthar, co-founder of Cottons B. Born in India, I've called the US home for over a decade. Passionate about pure organic cotton, I started Cottons B with my husband to raise awareness. Did you know your clothes can harm your health? We introduce you to a brand that cares about how clothing makes you feel!  We're on a mission to shift the fashion industry towards comfort and well-being. Each piece is carefully designed and custom-made for you. Join us in wearing clothes that feel good and do good!
+          Hi, I&#8217;m Nidhi Suthar, co-founder of Cottons B. Born in India, I&#8217;ve called the US home for over a decade. Passionate about pure organic cotton, I started Cottons B with my husband to raise awareness. Did you know your clothes can harm your health? We introduce you to a brand that cares about how clothing makes you feel!  We&#8217;re on a mission to shift the fashion industry towards comfort and well-being. Each piece is carefully designed and custom-made for you. Join us in wearing clothes that feel good and do good!
         </p>
     </div>
     )
--- app/components/BenefitsList.tsx
+++ app/components/BenefitsList.tsx
@@ -8,7 +8,7 @@ export default function BenefitsList() {
 
             <ul className=" leading-relaxed text-gray-500 xl:text-lg ml-4 mt-4">
                 <li className="py-2"><span className="font-bold">Breathability</span>: Cotton allows for airflow and moisture transfer, reducing sweat buildup and discomfort. <br /><span className="italic">- Journal of Textile Science and Technology</span></li>
-                <li className="py-2"><span className="font-bold">Comfort</span>: Cotton's softness and natural drape provide ergonomic comfort, reducing physiological stress. <br /><span className="italic">- Journal of Applied Physiology</span></li>
+                <li className="py-2"><span className="font-bold">Comfort</span>: Cottons softness and natural drape provide ergonomic comfort, reducing physiological stress. <br /><span className="italic">- Journal of Applied Physiology</span></li>
                 <li className="py-2"><span className="font-bold">Thermoregulation</span>: Cotton helps maintain a stable body temperature, ideal for hot and cold environments. <br /><span className="italic">- European Journal of Applied Physiology</span></li>
                 <li className="py-2"><span className="font-bold">Absorbency</span>: Cotton can absorb up to 27 times its weight in water, making it ideal for sweat management. <br /><span className="italic">- American Journal of Agricultural and Biological Sciences</span></li>
                 <li className="py-2"><span className="font-bold">Skin Health</span>: Cotton reduces skin irritation and inflammation due to its natural fibers and minimal chemical processing. <br /><span className="italic">- Journal of Investigative Dermatology</span></li>
